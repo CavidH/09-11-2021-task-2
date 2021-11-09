d07d813882dcf75f56cfafe75b64e30d0e69ddc6
CavidH <hcavid386@gmail.com>
2021-11-09 08:52:31 +0400

    Add project files.

---
+++ 09-11-2021-task-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09_11_2021_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
